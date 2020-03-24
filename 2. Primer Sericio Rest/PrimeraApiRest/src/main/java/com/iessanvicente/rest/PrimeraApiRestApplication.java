b3dcbad342caa65f394d13c821275b2272b0454a
Rueskas <Rueskas@hotmail.com>
2020-03-24 09:13:57 +0100

    Estructura Basica

---
+++ 2. Primer Sericio Rest/PrimeraApiRest/src/main/java/com/iessanvicente/rest/PrimeraApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.iessanvicente.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeraApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeraApiRestApplication.class, args);
+	}
+
+}
---
+++ 2. Primer Sericio Rest/PrimeraApiRest/src/main/java/com/iessanvicente/rest/models/ProductoRepositorio.java
@@ -0,0 +1,7 @@
+package com.iessanvicente.rest.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoRepositorio extends JpaRepository<Producto, Long> {
+
+}
