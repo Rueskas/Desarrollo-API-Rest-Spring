5e206eb44ab42dff8fe781e9cf7b1d96b57defd6
Rueskas <Rueskas@hotmail.com>
2020-03-24 08:42:20 +0100

    .

--- 09_EstructuraRutasBase/src/main/java/com/openwebinars/rest/modelo/ProductoRepositorio.java
+++
@@ -1,7 +0,0 @@
-package com.openwebinars.rest.modelo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepositorio extends JpaRepository<Producto, Long> {
-
-}
--- ZC_PildoraCorsServidor/src/main/java/com/openwebinars/rest/modelo/CategoriaRepositorio.java
+++
@@ -1,7 +0,0 @@
-package com.openwebinars.rest.modelo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepositorio extends JpaRepository<Categoria, Long> {
-
-}
